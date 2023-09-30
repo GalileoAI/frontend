3b21eb897686893980dc6a90a60d651751e6a817
Joanna Prajzendanc <j.prajzendanc@gmail.com>
2023-09-30 19:19:29 +0200

    feat: adding subpages "deklaracja dostępności" etc.

---
+++ src/app/components/deklaracja-dostepnosci/deklaracja-dostepnosci.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deklaracja-dostepnosci',
+  templateUrl: './deklaracja-dostepnosci.component.html',
+  styleUrls: ['./deklaracja-dostepnosci.component.scss']
+})
+export class DeklaracjaDostepnosciComponent {
+
+}
