0b083a58d963c5b6580a31c8e7aeed115b59abf7
Wojciech Marek <marek.wojciech@outlook.com>
2023-09-30 16:54:03 +0200

    feat: add gov bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HavePlanComponent } from './components/have-plan/have-plan.component';
 import { LandingComponent } from './components/landing/landing.component';
-import {SurpriseMeComponent} from "./components/surprise-me/surprise-me.component";
+import { SurpriseMeComponent } from './components/surprise-me/surprise-me.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'mam-plan', component: HavePlanComponent },
-  { path: 'zaskocz-mnie', component: SurpriseMeComponent},
+  { path: 'zaskocz-mnie', component: SurpriseMeComponent },
 ];
 
 @NgModule({
