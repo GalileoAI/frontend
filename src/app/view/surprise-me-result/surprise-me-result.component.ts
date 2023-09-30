95dbcd8bb92e5c8109983f07a529d60703eaf5ee
Joanna Prajzendanc <j.prajzendanc@gmail.com>
2023-10-01 00:26:55 +0200

    feat: working on listing the results

--- src/app/view/surprise-me-result/surprise-me-result.component.ts
+++ src/app/view/surprise-me-result/surprise-me-result.component.ts
@@ -1,14 +1,41 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {PositionClass} from "../../model/classes";
+import {ColumnMode, DatatableComponent} from '@swimlane/ngx-datatable';
 
 @Component({
   selector: 'app-surprise-me-result',
   templateUrl: './surprise-me-result.component.html',
   styleUrls: ['./surprise-me-result.component.scss']
 })
-export class SurpriseMeResultComponent {
+export class SurpriseMeResultComponent implements OnInit{
 
   @Input()
   positionsList: PositionClass[] = [];
+
+  ColumnMode = ColumnMode;
+
+  @ViewChild(DatatableComponent)
+  table: DatatableComponent = {} as DatatableComponent;
+
+  ngOnInit() {
+  }
+
+  updateFilter(event: any) {
+    const val = event.target.value.toLowerCase();
+
+    // filter our data
+    const temp = this.positionsList.filter((d) => {
+      return d.schools.filter((s) => {
+        return s.name.toLowerCase().indexOf(val) !== -1 ||
+          s.description.toLowerCase().indexOf(val) !== -1 ||
+          !val
+      });
+    });
+
+    // update the rows
+    this.positionsList = temp;
+    // Whenever the filter changes, always go back to the first page
+    this.table.offset = 0;
+  }
 
 }
